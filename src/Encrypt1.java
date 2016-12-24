ebf963be92f1bdc847faa67e8bdca003e41515ff
Angeline Tan <atan009@gmail.com>
2016-12-24 11:53:43 +0530

    Added decrypt method and tests.

--- src/Encrypt1.java
+++ src/Encrypt1.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by davidtan on 23/12/2016.
  */
@@ -32,4 +34,37 @@ public class Encrypt1 {
     }
 
 
+    public static String decrypt(String input, int numberOfDecryptionCycles) {
+
+        //create a fixed sized array from input length
+        char[] charArray = new char[input.length()];
+
+        //save first half of string into odd indexes in the the char array
+        for (int i = 0; i < input.length()/2; i++) {
+            charArray[i*2+1] = input.charAt(i);
+        }
+
+        //save second half of string into even indexes in the the char array
+        int j = input.length()/2; // 7
+        for (int i = 0; i < input.length(); i+=2) {
+            charArray[i] = input.charAt(j + i/2);
+        }
+
+        if (input == null && input.isEmpty()) {
+            return null;
+        }
+        else if (numberOfDecryptionCycles <= 0) {
+            return input;
+        } else if (numberOfDecryptionCycles == 1) {
+            String output = new String(charArray);
+            return output;
+        } else {
+            while (numberOfDecryptionCycles > 1){
+                numberOfDecryptionCycles--;
+                return decrypt(new String(charArray), numberOfDecryptionCycles);
+            }
+        }
+
+        return new String(charArray);
+    }
 }
--- test/Encrypt1Test.java
+++ test/Encrypt1Test.java
@@ -1,3 +1,4 @@
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -40,4 +41,46 @@ public class Encrypt1Test {
     public void test7() throws Exception {
         assertEquals("hskt svr neetn!Ti aai eyitrsig", Encrypt1.encrypt("This kata is very interesting!", 1));
     }
+
+    @Test
+    public void decryptNoCyclesTest() throws Exception {
+        assertEquals("This is a test!", Encrypt1.decrypt("This is a test!", 0));
+    }
+
+
+    @Test
+    public void decryptOneCycleTest() throws Exception {
+        assertEquals("This is a test!", Encrypt1.decrypt("hsi  etTi sats!", 1));
+
+    }
+
+    @Test
+    public void decryptTwoCyclesTest() throws Exception {
+        assertEquals("This is a test!", Encrypt1.decrypt("s eT ashi tist!", 2));
+
+    }
+
+    @Test
+    public void decryptThreeCyclesTest() throws Exception {
+        assertEquals("This is a test!", Encrypt1.decrypt(" Tah itse sits!", 3));
+
+    }
+
+    @Test
+    public void decryptFourCyclesTest() throws Exception {
+        assertEquals("This is a test!", Encrypt1.decrypt("This is a test!", 4));
+
+    }
+
+    @Test
+    public void decryptNegativeCyclesTest() throws Exception {
+        assertEquals("This is a test!", Encrypt1.decrypt("This is a test!", -1));
+
+    }
+    @Test
+    public void decryptOneCycleTest2() throws Exception {
+        assertEquals("This kata is very interesting!", Encrypt1.decrypt("hskt svr neetn!Ti aai eyitrsig", 1));
+
+    }
+
 }
